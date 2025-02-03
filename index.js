53711693c28c6c89368353f8e55f0cec8663c7b4
Odima-dev <vincentodima22@gmail.com>
2025-02-03 22:06:16 +0300

    if else if else statements

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-let firstCard = 3;
-let secondCard = 6;
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack!")
+    hasBlackJack = true;
+} else {
+    console.log("You're out of the game!")
+}
