26cd3c8f207dbc31c28bb9dc10120fa1cce8c3e3
Odima-dev <vincentodima22@gmail.com>
2025-02-04 12:34:04 +0300

    Adding start game button

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = 13;
+let secondCard = 6;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum; 
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂";
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳";
+        message = "You've got Blackjack!";
         hasBlackJack = true;
     } else {
-        message = "You're out of the game! 😭";
+        message = "You're out of the game!";
         isAlive = false;
     }
+    messageEl.textContent = message;
 }
 
-console.log(message)
+
