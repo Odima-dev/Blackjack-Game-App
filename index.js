6d44ddb880be3caabcd8c6374cf2af3645bcd862
Odima-dev <vincentodima22@gmail.com>
2025-02-05 12:32:24 +0300

    using math.random method

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
+let cards = []
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function getRandomCard() {
-    return (Math.floor(Math.random()*13)) + 1;
-}
+    let randomNumber = (Math.floor(Math.random()*13)) + 1;
+    if (randomNumber > 10) {
+        return 11
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+} 
 
 function startGame () {
+    isAlive = true;
+    let firstNumber = getRandomCard();
+    let secondNumber = getRandomCard();
+    cards = [firstNumber, secondNumber];
+    sum = firstNumber + secondNumber;
+
     renderGame();
 }
 
